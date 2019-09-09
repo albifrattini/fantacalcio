e9860a3809625bd785a35b86f0879424e2af7b6d
albi_frattini <alberto.frattini@mail.polimi.it>
2019-09-09 18:18:12 +0200

    Working on heroku

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ function initPeople() {
 	return dbConnection.schema.hasTable('people').then(exists => {
 		if(!exists) {
 			dbConnection.schema.createTable('people', table => {
-				table.increments();
-				table.string('name');
+				table.increments().primary();
+				table.string('ownerName');
 				table.integer('ownerId');
 			})
 			.then(function() {
@@ -53,49 +53,23 @@ function initPeople() {
 }
 
 function initPlayers() {
+	let playersList = require('./other/initPlayers.json');
 
 	return dbConnection.schema.hasTable('players').then(exists => {
 		if(!exists) {
 			dbConnection.schema.createTable('players', table => {
-				table.increments();
-				table.string('name');
-				table.string('ownerId').references('ownerId').inTable('people');
+				table.increments().primary();
+				table.string('playerName');
+				table.integer('ownerId');
 				table.integer('price');
 				table.enum('role', ['Portiere', 'Difensore', 'Centrocampista', 'Attaccante']);
+			})
+			.then(function() {
+				return dbConnection('players').insert(playersList);
 			});
 		} 
 	})
 }
-
-
-
-
-
-
-
-
-
-app.get('/', (request, response) => {
-	
-});
-
-app.get('/players', (request, response) => {
-
-});
-
-app.get('/players/:id', (request, response) => {
-
-});
-
-app.post('/players/:id', (request, response) => {
-
-});
-
-app.delete('/players/:id', (request, response) => {
-
-});
-
-
 
 
 
@@ -110,6 +84,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+
+
+
+
+
+app.get('/v2/people', (request, response) => {
+	let allPeople = dbConnection('people');
+	allPeople.then(result => {
+		response.json(result);
+	});
+});
+
+app.get('/v2/players', (request, response) => {
+	let allPlayers = dbConnection('players');
+	allPlayers = allPlayers.join('people', 'players.ownerId', 'people.ownerId');
+	allPlayers.then(result => {
+		response.json(result);
+	});
+});
+
+app.post('/v2/players', (request, response) => {
+	let newPlayer = {
+		playerName : request.body.playerName,
+		ownerId : parseInt(request.body.ownerId),
+		price : parseInt(request.body.price),
+		role : request.body.role,
+	};
+	dbConnection('players').insert(newPlayer).then(result => {
+		response.send(`${newPlayer.playerName} inserito!`);
+	});
+});
+
+app.delete('/v2/players/:name', (request, response) => {
+	let deletedPlayer = request.params.name;
+	dbConnection('players').where('playerName', deletedPlayer).del().then((_) => {
+		response.send(`${deletedPlayer} è stato eliminato!`);
+	});
+});
+
+
+
+
+
+
 
 initDb();
 
---
+++ public/assets/js/home.js
@@ -0,0 +1,179 @@
+var budgetAvailable = {};
+
+function getPeople() {
+	fetch('/v2/people')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (people) {
+			people.map(disposePeople);
+		});
+}
+
+function getPlayers() {
+	fetch('/v2/players')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (players) {
+			players.map(disposePlayers);
+		});
+}
+
+function disposePlayers(player) {
+
+	let role = player.role;
+	budgetAvailable[`${player.ownerName}`] = budgetAvailable[`${player.ownerName}`] - player.price;
+	document.getElementById(`totale-${player.ownerName}`).innerHTML = budgetAvailable[`${player.ownerName}`];
+
+	if(role == 'Portiere') {
+		$(`#portieri-${player.ownerName}`).append(
+			`
+				<div class="row">
+					<div class="col-md-2">
+						<button class="btn" onclick="deletePlayer(this.id)" id="${player.playerName}">
+    						<span class="glyphicon glyphicon-trash" style="color: lightgray;"></span>
+  						</button>
+					</div>
+					<div class="col-md-7">
+						<h5>${player.playerName}</h5>
+					</div>
+					<div class="col-md-3">
+						<h5>${player.price}</h5>
+					</div>
+				</div>
+			`
+		);
+	} else if(role == 'Difensore') {
+		$(`#difensori-${player.ownerName}`).append(
+			`
+				<div class="row">
+					<div class="col-md-2">
+						<button class="btn" onclick="deletePlayer(this.id)" id="${player.playerName}">
+    						<span class="glyphicon glyphicon-trash" style="color: lightgray;"></span>
+  						</button>
+					</div>
+					<div class="col-md-7">
+						<h5>${player.playerName}</h5>
+					</div>
+					<div class="col-md-3">
+						<h5>${player.price}</h5>
+					</div>
+				</div>
+			`
+		);
+	} else if(role == 'Centrocampista') {
+		$(`#centrocampisti-${player.ownerName}`).append(
+			`
+				<div class="row">
+					<div class="col-md-2">
+						<button class="btn" onclick="deletePlayer(this.id)" id="${player.playerName}">
+    						<span class="glyphicon glyphicon-trash" style="color: lightgray;"></span>
+  						</button>
+					</div>
+					<div class="col-md-7">
+						<h5>${player.playerName}</h5>
+					</div>
+					<div class="col-md-3">
+						<h5>${player.price}</h5>
+					</div>
+				</div>
+			`
+		);
+	} else if(role == 'Attaccante') {
+		$(`#attaccanti-${player.ownerName}`).append(
+			`
+				<div class="row">
+					<div class="col-md-2">
+						<button class="btn" onclick="deletePlayer(this.id)" id="${player.playerName}">
+    						<span class="glyphicon glyphicon-trash" style="color: lightgray;"></span>
+  						</button>
+					</div>
+					<div class="col-md-7">
+						<h5>${player.playerName}</h5>
+					</div>
+					<div class="col-md-3">
+						<h5>${player.price}</h5>
+					</div>
+				</div>
+			`
+		);
+	}
+}
+
+function disposePeople(person) {
+
+	budgetAvailable[`${person.ownerName}`] = 500;
+
+	$('#people-players').append(
+		`
+			<div class="col-md-3">
+				<h1>${person.ownerName}</h1>
+				<hr>
+				<div id="portieri-${person.ownerName}">
+					<h4 style="color: Orange;">Portieri</h4>
+				</div>
+				<hr>
+				<div id="difensori-${person.ownerName}">
+					<h4 style="color: MediumSeaGreen;">Difensori</h4>
+				</div>
+				<hr>
+				<div id="centrocampisti-${person.ownerName}">
+					<h4 style="color: DodgerBlue;">Centrocampisti</h4>
+				</div>
+				<hr>
+				<div id="attaccanti-${person.ownerName}">
+					<h4 style="color: Tomato;">Attaccanti</h4>
+				</div>
+				<hr>
+				<div class="row">
+					<div class= "col-md-9">
+						<h4 style="color: gray">Totale</h4>
+					</div>
+					<div class="col-md-3">
+						<h4 id="totale-${person.ownerName}">500</h4>
+					</div>
+				</div>
+			</div>
+
+		`
+	);
+}
+
+function addPlayer() {
+	var newPlayer = {
+		playerName : document.getElementById('playerName').value,
+		role : document.getElementById('roleSelector').value,
+		price : document.getElementById('priceInput').value,
+		ownerId : document.getElementById('ownerSelector').value
+	};
+
+	fetch('/v2/players', {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(newPlayer)
+	}).then((response) => {
+		response.text().then((text) => {
+			alert(text);
+		});
+	});
+	location.reload();
+}
+
+function deletePlayer(plName) {
+	fetch(`/v2/players/${plName}`, {
+		method: 'DELETE'
+	}).then((response) => {
+		response.text().then((text) => {
+			alert(text);
+		});
+	});
+	location.reload();
+}
+
+
+getPeople();
+getPlayers();
