74ce0d639eb629ea4f4685831697ce209cf0d0f6
albi_frattini <alberto.frattini@mail.polimi.it>
2019-09-09 18:37:51 +0200

    Prova iphone 3

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -127,10 +127,10 @@ function disposePeople(person) {
 				</div>
 				<hr>
 				<div class="row">
-					<div class= "col-md-9">
+					<div class= "col-md-9 col-sm-9 col-xs-9">
 						<h4 style="color: gray">Totale</h4>
 					</div>
-					<div class="col-md-3">
+					<div class="col-md-3 col-sm-3 col-xs-9">
 						<h4 id="totale-${person.ownerName}">500</h4>
 					</div>
 				</div>
